6f649c23d5adb32acced44a122889491a1be53ec
XstylelouisX <louis80193@gmail.com>
2020-03-05 15:42:49 +0800

    Update GenericsV2

--- Assets/Script/GenericsV2.cs
+++ Assets/Script/GenericsV2.cs
@@ -6,9 +6,12 @@ public class GenericsV2 : MonoBehaviour
 {
     private void Start()
     {
-        Tester.Start();
+        //Tester.Start();
+        //Program.Start();
+        TestDelegate.Start();
     }
 
+    //泛型(Generic)基礎
     public class MyGenericArray<T>
     {
         private T[] array;
@@ -60,6 +63,79 @@ public class GenericsV2 : MonoBehaviour
             {
                 Debug.Log(charArray.getItem(i) + " ");
             }
+        }
+    }
+
+    //泛型(Generic)方法
+    class Program
+    {
+        private static void Swap<T>(ref T type1, ref T type2)
+        {
+            T temp;
+            temp = type1;
+            type1 = type2;
+            type2 = temp;
+        }
+
+        public static void Start()
+        {
+            int a, b;
+            char c, d;
+            a = 10;
+            b = 20;
+            c = 'I';
+            d = 'V';
+
+            Debug.Log("交換前數值:");
+            Debug.Log(a);
+            Debug.Log(b);
+            Debug.Log("交換前字符:");
+            Debug.Log(c);
+            Debug.Log(d);
+
+            //交換
+            Swap<int>(ref a, ref b);
+            Swap<char>(ref c, ref d);
+
+            Debug.Log("交換後數值:");
+            Debug.Log(a);
+            Debug.Log(b);
+            Debug.Log("交換後字符:");
+            Debug.Log(c);
+            Debug.Log(d);
+        }
+    }
+
+    //泛型(Generic)委託
+    class TestDelegate
+    {
+        delegate T NumberChanger<T>(T n);
+        private static int num = 10;
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+
+        public static int getNum()
+        {
+            return num;
+        }
+
+        public static void Start()
+        {
+            NumberChanger<int> nc1 = new NumberChanger<int>(AddNum);
+            NumberChanger<int> nc2 = new NumberChanger<int>(MultNum);
+            nc1(25);
+            Debug.Log("加總和:" + getNum());
+            nc2(5);
+            Debug.Log("乘總和:" + getNum());
         }
     }
 }
