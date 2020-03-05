b69a992d73ef2012af9b1f114396c221ef84f180
XstylelouisX <louis80193@gmail.com>
2020-03-05 13:56:38 +0800

    Upload Lambda

---
+++ Assets/Script/Lambda.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Lambda : MonoBehaviour
+{
+    private void Start()
+    {
+        LambdaSort();
+        LambdaCalculation();
+    }
+
+    //排序
+    private void LambdaSort()
+    {
+        int[] number = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        //從小排到大(編程式查詢)
+        IEnumerable<int> results = number.Where(x => x > 4).OrderBy(y => y);
+
+        foreach (var item in results)
+        {
+            Debug.Log("編程式查詢:" + item.ToString());
+        }
+    }
+
+    //計算
+    private void LambdaCalculation()
+    {
+        int[] number = { 2, 3, 4, 5 };
+        var squaredNumbers = number.Select(x => x * x);
+        //插入分隔顯示
+        Debug.Log(string.Join(" ", squaredNumbers));
+    }
+}
--- Assets/Script/Linq.cs
+++ Assets/Script/Linq.cs
@@ -9,7 +9,6 @@ public class Linq : MonoBehaviour
     {
         DelayLoad();
         FirstLoad();
-        Lambda();
     }
 
     //延遲載入(執行foreach取出每一個元素都會執行LINQ)
@@ -37,19 +36,6 @@ public class Linq : MonoBehaviour
         foreach (var item in loadedQuery)
         {
             Debug.Log("預先載入:" + item.ToString());
-        }
-    }
-
-    //編程式查詢
-    private void Lambda()
-    {
-        int[] number = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        //從小排到大
-        IEnumerable<int> results = number.Where(x => x > 4).OrderBy(y => y);
-
-        foreach (var item in results)
-        {
-            Debug.Log("編程式查詢:" + item.ToString());
         }
     }
 }
