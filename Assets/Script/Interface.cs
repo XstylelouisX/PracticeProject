afd6771cd6e1487b7eb0185c0f38e445a07fecc7
XstylelouisX <louis80193@gmail.com>
2020-03-06 09:38:54 +0800

    Upload Interface

---
+++ Assets/Script/Interface.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interface : MonoBehaviour
+{
+    interface ISampleInterface
+    {
+        void SampleMethod();
+    }
+
+    private void Start()
+    {
+        ImplementationClass.Start();
+        TestClass.Start();
+    }
+
+    class TestClass : ISampleInterface
+    {
+        void ISampleInterface.SampleMethod()
+        {
+            Debug.Log("測試類別");
+        }
+        public static void Start()
+        {
+            ISampleInterface obj = new TestClass();
+            obj.SampleMethod();
+        }
+    }
+
+    class ImplementationClass : ISampleInterface
+    {
+        void ISampleInterface.SampleMethod()
+        {
+            Debug.Log("簡單方法");
+        }
+
+        public static void Start()
+        {
+            ISampleInterface obj = new ImplementationClass();
+            obj.SampleMethod();
+        }
+    }
+}
